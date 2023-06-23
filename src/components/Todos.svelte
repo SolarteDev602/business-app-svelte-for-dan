7a2b5503dea98c4a9b4449b0aaf06afb9f957607
binary3oul <solartacostaw@gmail.com>
2023-06-23 08:20:05 -0500

    update filtertood

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,31 +1,41 @@
 <script>
-	export let todos = []
+	export let todos = [];
 
 	$: totalTodos = todos.length;
 	$: completedTodos = todos.filter((todo) => todo.completed).length;
-	$: newTodoName = ""
-	let filter = "all"
+	$: newTodoName = "";
+	let filter = "all";
 	function addTodo() {
-		if(newTodoName) 
-			todos = [...todos, {
-				id: todos.reduce((maxId, {id}) => maxId > id ? maxId : id, 0) + 1, 
-				name: newTodoName, 
-				completed: false}]
-		newTodoName = ""
+		if (newTodoName)
+			todos = [
+				...todos,
+				{
+					id:
+						todos.reduce(
+							(maxId, { id }) => (maxId > id ? maxId : id),
+							0
+						) + 1,
+					name: newTodoName,
+					completed: false,
+				},
+			];
+		newTodoName = "";
 	}
 	function checkAll(value) {
-		return todos.map(todo => ({...todo, completed: value}))
+		return todos.map((todo) => ({ ...todo, completed: value }));
 	}
 	function markToCheckAll() {
-		todos = checkAll(true)
+		todos = checkAll(true);
 	}
 	function markToUncheckAll() {
-		todos = checkAll(false)
+		todos = checkAll(false);
 	}
-	const filterTodos = (filter) => 
-		filter == "active" ? todos.filter (t => !t.completed) :
-		filter === "completed" ? todos.filter (t => t.completed) :
-		todos
+	const filterTodos = (filter, todos) =>
+		filter == "active"
+			? todos.filter((t) => !t.completed)
+			: filter === "completed"
+			? todos.filter((t) => t.completed)
+			: todos;
 </script>
 
 <h1>Todo List</h1>
@@ -33,9 +43,17 @@
 	<!-- NewTodo -->
 	<form on:submit|preventDefault={addTodo}>
 		<h2 class="label-wrapper">
-			<label for="todo-0" class="label__lg"> What needs to be done? </label>
+			<label for="todo-0" class="label__lg">
+				What needs to be done?
+			</label>
 		</h2>
-		<input type="text" id="todo-0" autocomplete="off" class="input input__lg" bind:value={newTodoName}/>
+		<input
+			type="text"
+			id="todo-0"
+			autocomplete="off"
+			class="input input__lg"
+			bind:value={newTodoName}
+		/>
 		<button type="submit" disabled="" class="btn btn__primary btn__lg">
 			Add
 		</button>
@@ -43,17 +61,38 @@
 
 	<!-- Filter -->
 	<div class="filters btn-group stack-exception">
-		<button class="btn toggle-btn" aria-pressed={filter === 'all'} class:btn__primary={filter === 'all'} on:click={() => {filter = "all"}}>
+		<button
+			class="btn toggle-btn"
+			aria-pressed={filter === "all"}
+			class:btn__primary={filter === "all"}
+			on:click={() => {
+				filter = "all";
+			}}
+		>
 			<span class="visually-hidden">Show</span>
 			<span>All</span>
 			<span class="visually-hidden">tasks</span>
 		</button>
-		<button class="btn toggle-btn" aria-pressed={filter === 'active'} class:btn__primary={filter === 'active'} on:click={() => {filter = "active"}}>
+		<button
+			class="btn toggle-btn"
+			aria-pressed={filter === "active"}
+			class:btn__primary={filter === "active"}
+			on:click={() => {
+				filter = "active";
+			}}
+		>
 			<span class="visually-hidden">Show</span>
 			<span>Active</span>
 			<span class="visually-hidden">tasks</span>
 		</button>
-		<button class="btn toggle-btn" aria-pressed={filter === 'completed'} class:btn__primary={filter === 'completed'} on:click={() => {filter = "completed"}}>
+		<button
+			class="btn toggle-btn"
+			aria-pressed={filter === "completed"}
+			class:btn__primary={filter === "completed"}
+			on:click={() => {
+				filter = "completed";
+			}}
+		>
 			<span class="visually-hidden">Show</span>
 			<span>Completed</span>
 			<span class="visually-hidden">tasks</span>
@@ -66,29 +105,48 @@
 	</h2>
 
 	<!-- Todos -->
-	<ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-		{#each filterTodos(filter) as todo (todo.id)}
+	<ul
+		role="list"
+		class="todo-list stack-large"
+		aria-labelledby="list-heading"
+	>
+		{#each filterTodos(filter, todos) as todo (todo.id)}
 			<li class="todo">
 				<div class="stack-small">
 					<div class="c-cb">
 						<input
 							type="checkbox"
 							id="todo-{todo.id}"
-							checked={todo.completed} 
-							on:click = {() => todo.completed = !todo.completed}/>
-						<label for="todo-{todo.id}" class="todo-label"> {todo.name} </label>
+							checked={todo.completed}
+							on:click={() => {
+								todo.completed = !todo.completed;
+								console.log("completedTodos", completedTodos);
+							}}
+						/>
+						<label for="todo-{todo.id}" class="todo-label">
+							{todo.name}
+						</label>
 					</div>
 					<div class="btn-group">
 						<button type="button" class="btn">
-							Edit <span class="visually-hidden">{todo.name}</span>
+							Edit <span class="visually-hidden">{todo.name}</span
+							>
 						</button>
-						<button type="button" class="btn btn__danger" on:click={(e) => {todos = todos.filter(td => td.id !== todo.id)}}>
-							Delete <span class="visually-hidden">{todo.name}</span>
+						<button
+							type="button"
+							class="btn btn__danger"
+							on:click={(e) => {
+								todos = todos.filter((td) => td.id !== todo.id);
+							}}
+						>
+							Delete <span class="visually-hidden"
+								>{todo.name}</span
+							>
 						</button>
 					</div>
 				</div>
 			</li>
-			{:else}
+		{:else}
 			<li>Nothing to do here!</li>
 		{/each}
 	</ul>
@@ -97,7 +155,13 @@
 
 	<!-- MoreActions -->
 	<div class="btn-group">
-		<button type="button" class="btn btn__primary" on:click={markToCheckAll}>Check all</button>
-		<button type="button" class="btn btn__primary" on:click={markToUncheckAll}>Remove completed</button>
+		<button type="button" class="btn btn__primary" on:click={markToCheckAll}
+			>Check all</button
+		>
+		<button
+			type="button"
+			class="btn btn__primary"
+			on:click={markToUncheckAll}>Remove completed</button
+		>
 	</div>
 </div>
